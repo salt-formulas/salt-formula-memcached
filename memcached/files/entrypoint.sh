357b7913c6d2fc6fc79d640338682e8d10f508ed
Filip Pytloun <filip@pytloun.cz>
2016-05-25 12:23:22 +0200

    Fix entrypoint

--- memcached/files/entrypoint.sh
+++ memcached/files/entrypoint.sh
@@ -3,6 +3,7 @@
 
 salt-call --local --retcode-passthrough state.highstate
 service {{ server.service }} stop || true
+pkill -u memcache
 
 /usr/bin/memcached -m {{ server.cache_size }} \
     -l {{ server.bind.address }} -p {{ server.bind.port }} -U {{ server.bind.port }} \
